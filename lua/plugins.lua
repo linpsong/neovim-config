e26fc90efb698d6570ec9ab9c382427ad9fc4820
linpsong <lpsersci@gmail.com>
2023-10-03 00:34:21 +0800

    add null-ls.nvim config: clang_format and cmake_format

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -95,6 +95,8 @@ require("lazy").setup({
         sources = {
           null_ls.builtins.formatting.stylua,
           null_ls.builtins.formatting.black,
+          null_ls.builtins.formatting.clang_format,
+          null_ls.builtins.formatting.cmake_format,
           null_ls.builtins.diagnostics.eslint,
           null_ls.builtins.completion.spell,
         },
