4dea6272a6486a71eafc539fda9e398f212aba72
linpsong <lpsersci@gmail.com>
2024-06-26 12:48:16 +0800

    Hide annoying diagnostics reminder by using virtual_text attribute.

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -93,6 +93,16 @@ function Plugin.config()
 			end,
 		},
 	})
+
+	local function setup_lsp_diags()
+		vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+			virtual_text = false,
+			signs = true,
+			update_in_insert = false,
+			underline = true,
+		})
+	end
+	setup_lsp_diags()
 end
 
 function user.on_attach(event)
